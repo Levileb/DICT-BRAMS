40da5b25d23628fa572ed2dae9181c648c9e3739
Levileb <127000037+Levileb@users.noreply.github.com>
2025-02-12 12:11:55 +0800

    househols-add-member-check

--- household-profile.js
+++ household-profile.js
@@ -235,3 +235,10 @@
     residentsRef.on('child_changed', fetchResidents);
     residentsRef.on('child_removed', fetchResidents);
     residentsRef.on('child_added', fetchResidents);
+
+
+    function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+}
