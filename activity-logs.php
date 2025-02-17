c162ec05582959d1287ffcfbbeb2c18ebfbb0831
Nicqol <lapidezmarynicholeit2011@gmail.com>
2025-02-17 11:28:37 +0800

    ui-fix

--- activity-logs.php
+++ activity-logs.php
@@ -53,7 +53,7 @@ body::-webkit-scrollbar {
     <?php include_once 'Includes/admin-navbar.php'; ?>
 
     <div class="wrapper">
-        <div id="activity-logs" class="tab-content" style="margin-top: 100px;">
+        <div id="activity-logs" class="tab-content" style="margin-top: 10px;">
             <div class="inner-container p-6 w-full" style="margin-top: 1rem;">
                 <h2 class="text-2xl font-bold text-gray-700" style="padding: 1rem;">ACTIVITY LOG</h2>
                 <div class="bg-white rounded-lg shadow-md p-6 overflow-x-auto">
