99e2f7b697f0ab2387b719dd60db7fbe91c19ad5
trsh <escartin.trishagit2011@gmail.com>
2025-02-18 09:46:48 +0800

    minor

--- registered-residents.php
+++ registered-residents.php
@@ -44,7 +44,7 @@ body::-webkit-scrollbar {
 
     <div class = "wrapper">
         <div id="archive-log" class="tab-content">
-            <div class="inner-container p-6 w-full" style="margin-top: 50px;">
+            <div class="inner-container p-6 w-full" style="margin-top: 10px;">
                 <h2 class="text-2xl font-bold text-gray-700" style="padding: 1rem;">REGISTERED RESIDENT LOG</h2>
                 <div class="bg-white rounded-lg shadow-md p-6 overflow-x-auto">
                     <table class="w-full border-collapse border border-gray-300 text-sm text-gray-700">
