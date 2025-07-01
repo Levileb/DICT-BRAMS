755c04f9c0c0ffbd3123cca69777294ec095af6f
Levileb <levijohn.ledesma@wvsu.edu.ph>
2025-07-01 10:31:52 +0800

    fix-bug-exp-excel

--- resident_list.php
+++ resident_list.php
@@ -188,10 +188,7 @@
             const month = String(date.getMonth() + 1).padStart(2, '0');
             const day = String(date.getDate()).padStart(2, '0');
             const year = date.getFullYear();
-
-        // Create the popup container
-        const popup = document.createElement('div');
-        popup.classList.add('absolute', 'inset-0', 'bg-gray-800', 'bg-opacity-50', 'flex', 'items-center', 'justify-center', 'z-50');
+            const fileName = `Residents_List_${year}-${month}-${day}.xlsx`;
 
             const worksheet = XLSX.utils.json_to_sheet(residentsData);
             const workbook = XLSX.utils.book_new();
